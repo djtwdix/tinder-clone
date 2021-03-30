18999e21a569d331498203ed600a3b463700ea90
djtwdix <djtwd.ix@icloud.com>
2021-03-30 15:46:48 -0300

    Import header to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <h1>Tinder Clone</h1>
+      <Header />
+      {/* header */}
+      {/* card */}
+      {/* buttons */}
+
+      {/* chat screen */}
+      {/* individual chat screen */}
+      {/* profile screen */}
     </div>
   );
 }
