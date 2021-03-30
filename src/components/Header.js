81adc8df14c0c8cd9f967787096dd7b80983d071
djtwdix <djtwd.ix@icloud.com>
2021-03-30 16:24:09 -0300

    Create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
 
 export default function Header() {
   return (
-    <nav>
-      <h1>I am a header</h1>
+    <nav className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg"
+        alt="logo"
+      ></img>
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </nav>
   );
 }
