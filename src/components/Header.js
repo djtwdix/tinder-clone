ff96886d252749a8b539661066a6f44595d54b51
djtwdix <djtwd.ix@icloud.com>
2021-03-31 08:00:51 -0300

    Add arrow back button for messages

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,35 @@ import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { Link, useHistory } from "react-router-dom";
 
-export default function Header() {
+export default function Header({ backButton }) {
+  const history = useHistory()
+
   return (
     <nav className="header">
-      <IconButton>
-        <PersonIcon fontSize="large" className="header__icon" />
-      </IconButton>
-      <img
-        className="header__logo"
-        src="https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg"
-        alt="logo"
-      ></img>
-      <IconButton>
-        <ForumIcon fontSize="large" className="header__icon" />
-      </IconButton>
+      {backButton ? (
+          <IconButton onClick={() => history.replace(backButton)}>
+            <ArrowBackIosIcon fontSize="large" className="header__icon" />
+          </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      )}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg"
+          alt="logo"
+        ></img>
+      </Link>
+      <Link to="/chat">
+        <IconButton>
+          <ForumIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      </Link>
     </nav>
   );
 }
